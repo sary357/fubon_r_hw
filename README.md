fc1c0f3a5e77eca6d9c6c421813ac78503fdfc09
sary357 <sary357@gmail.com>
2017-09-29 16:50:59 +0800

    update all data for R report

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 ## 檔案說明
+- taipei_stolen_map.Rmd: 用來產生 R markdown 的報告使用
 - 臺北市10401-10608住宅竊盜點位資訊.csv: 已加上 lat/lon
 - stolen_in_taipei_10401_10608.csv: 和  臺北市10401-10608住宅竊盜點位資訊.csv 一模一樣, 只是用英文檔名
 - getLatLon_for_stolen_addr.py: 抓 臺北市10401-10608住宅竊盜點位資訊.csv 的Lat/Lon
 - getLatLon_forA_Lvr.py: 抓實價登錄的 Lat/Lon, 這邊只會針對土地+房+車位(車位是可以可無) 的交易類別做轉換
-- stolen_pricing.R: 針對住宅竊盜產生一些資訊並放在地圖上
+- stolen_pricing.R: 針對住宅竊盜產生一些資訊並放在地圖上, 也有簡單的統計數據, 如果想要單純跑看看, 可以一行一行執行看結果
+
+## 如何設定
+- 請修改 taipei_stolen_map.Rmd 的
+
+```
+shp_file_name="/Users/sary357/Downloads/R_Report/TOWN_MOI_1060831.shp"
+data_file_name="/Users/sary357/Downloads/R_Report/stolen_in_taipei_10401_10608.csv"
+```
+- data_file_name: 請寫明 stolen_in_taipei_10401_10608.csv 本機的絕對路徑
+- shp_file_name: 請到 TOWN_MOI_1060831.shp 本機的絕對路徑
+- 在 RStudio 執行 "Run Presentation" 即可
--- stolen_pricing.R
+++ stolen_pricing.R
@@ -1,82 +1,82 @@
-install.packages("ggmap") 
-install.packages("mapproj")
-install.packages("ggplot2")
+#install.packages("ggmap") 
+#install.packages("mapproj")
+#install.packages("ggplot2")
 library(ggmap)
 library(mapproj)
+library(spdep)
+library(maptools)
+library(magrittr)
+library(dplyr)
+
+shp_file_name="/Users/sary357/Downloads/R_Report/TOWN_MOI_1060831.shp"
+data_file_name="/Users/sary357/Downloads/R_Report/stolen_in_taipei_10401_10608.csv"
+
+plot_stolen_in_each_district_in_taipei=function(map_type, data_set, boarder_points, district_name){
+  #district_full_name
+  plotDf<-data_set %>% select(.,district, lat, lon) %>% filter(.,district==district_name) 
+  plot_area<- boarder_points[boarder_points@data$TOWNNAME==district_name,]
+  get_map(location = c(lon=median(plotDf$lon),lat=median(plotDf$lat)),
+                 source = "google", 
+                 zoom = 14,
+                 language = "zh-TW", 
+                 maptype = map_type) %>% 
+  ggmap() +geom_polygon(aes(x = long, y = lat), 
+                           data = plot_area,size=0.001, color="black",alpha=0.2)+stat_density2d(data = plotDf, aes(x = lon, y=lat,fill = ..level.., alpha = ..level..),size = 0.01, bins = 16, geom = "polygon") +scale_fill_gradient(low = "green", high = "red",guide = FALSE)+scale_alpha(range = c(0, 0.5), guide = FALSE)
+  
+  
+}
+
+# load data
+maptype<-"roadmap"
+file_name=data_file_name
+stolen_point <- read.csv(file_name)
+tw.map <- maptools::readShapeSpatial(shp_file_name)
+taipei<-tw.map[tw.map@data$COUNTYNAME=="臺北市",] 
 
 # Taipei: all district
-file_name="/Users/sary357/Downloads/R報告/stolen_in_taipei_10401_10608.csv"
-stolen_point <- read.csv(file_name)
+# 台北市全區住宅竊盜熱點圖
 map <- get_map(location = c(lon=median(stolen_point$lon),lat=median(stolen_point$lat)),
                source = "google", 
                zoom = 12,
                language = "zh-TW", 
-               maptype = "roadmap")
+               maptype = maptype)
 #ggmap(map) + geom_point(aes(x = lon, y = lat), data = stolen_point,size=1, col="red",alpha=0.6)+stat_density2d(data = stolen_point, aes(x = lon, y=lat,fill = ..level.., alpha = ..level..),size = 0.01, bins = 16, geom = "polygon") +scale_fill_gradient(low = "green", high = "red",guide = FALSE)+scale_alpha(range = c(0, 0.5), guide = FALSE)
-# 台北市全區住宅竊盜熱點圖
-ggmap(map) +stat_density2d(data = stolen_point, aes(x = lon, y=lat,fill = ..level.., alpha = ..level..),size = 0.01, bins = 16, geom = "polygon") +scale_fill_gradient(low = "green", high = "red",guide = FALSE)+scale_alpha(range = c(0, 0.5), guide = FALSE)
+ggmap(map) +geom_point(aes(x = long, y = lat), 
+                         data = taipei,size=0.001, color="black",alpha=0.1)+stat_density2d(data = stolen_point, aes(x = lon, y=lat,fill = ..level.., alpha = ..level..),size = 0.01, bins = 16, geom = "polygon") +scale_fill_gradient(low = "green", high = "red",guide = FALSE)+scale_alpha(range = c(0, 0.5), guide = FALSE)
 
 # generate according to different districts 
 addr<-stolen_point$定位地點
 district_lat<-stolen_point$lat
 district_lon<-stolen_point$lon
-district<-substr(addr, 1, 6)
+district<-substr(addr, 4, 6)
 df<-data.frame("district"=district, "lat"=district_lat, "lon"=district_lon)
-library(magrittr)
-library(dplyr)
-df$district
-df_1<-df %>% select(.,district, lat, lon) %>% filter(.,district=='台北市中山區') 
-df_2<-df %>% select(.,district, lat, lon) %>% filter(.,district=='台北市中正區') 
-df_3<-df %>% select(.,district, lat, lon) %>% filter(.,district=='台北市大同區') 
-df_4<-df %>% select(.,district, lat, lon) %>% filter(.,district=='台北市萬華區') 
-df_5<-df %>% select(.,district, lat, lon) %>% filter(.,district=='台北市松山區') 
-df_6<-df %>% select(.,district, lat, lon) %>% filter(.,district=='台北市大安區') 
-df_7<-df %>% select(.,district, lat, lon) %>% filter(.,district=='台北市信義區') 
-df_8<-df %>% select(.,district, lat, lon) %>% filter(.,district=='台北市文山區') 
-df_9<-df %>% select(.,district, lat, lon) %>% filter(.,district=='台北市南港區') 
-df_10<-df %>% select(.,district, lat, lon) %>% filter(.,district=='台北市內湖區') 
-df_11<-df %>% select(.,district, lat, lon) %>% filter(.,district=='台北市士林區')
-df_12<-df %>% select(.,district, lat, lon) %>% filter(.,district=='台北市北投區')
 
-# df_1: 台北市中山區
-map <- get_map(location = c(lon=median(df_1$lon),lat=median(df_1$lat)),
-               source = "google", 
-               zoom = 14,
-               language = "zh-TW", 
-               maptype = "roadmap")
-ggmap(map) +stat_density2d(data = df_1, aes(x = lon, y=lat,fill = ..level.., alpha = ..level..),size = 0.01, bins = 16, geom = "polygon") +scale_fill_gradient(low = "green", high = "red",guide = FALSE)+scale_alpha(range = c(0, 0.5), guide = FALSE)
+# let's plot
+district_name='中山區'
+plot_stolen_in_each_district_in_taipei(maptype,df,taipei,district_name)
+district_name='中正區'
+plot_stolen_in_each_district_in_taipei(maptype,df,taipei,district_name)
+district_name='大同區'
+plot_stolen_in_each_district_in_taipei(maptype,df,taipei,district_name)
+district_name='萬華區'
+plot_stolen_in_each_district_in_taipei(maptype,df,taipei,district_name)
+district_name='松山區'
+plot_stolen_in_each_district_in_taipei(maptype,df,taipei,district_name)
+district_name='大安區'
+plot_stolen_in_each_district_in_taipei(maptype,df,taipei,district_name)
+district_name='信義區'
+plot_stolen_in_each_district_in_taipei(maptype,df,taipei,district_name)
+district_name='中山區'
+plot_stolen_in_each_district_in_taipei(maptype,df,taipei,district_name)
+district_name='南港區'
+plot_stolen_in_each_district_in_taipei(maptype,df,taipei,district_name)
+district_name='內湖區'
+plot_stolen_in_each_district_in_taipei(maptype,df,taipei,district_name)
+district_name='士林區'
+plot_stolen_in_each_district_in_taipei(maptype,df,taipei,district_name)
+district_name='北投區'
+plot_stolen_in_each_district_in_taipei(maptype,df,taipei,district_name)
 
-# df_3: 台北市大同區
-map <- get_map(location = c(lon=median(df_3$lon),lat=median(df_3$lat)),
-               source = "google", 
-               zoom = 14,
-               language = "zh-TW", 
-               maptype = "roadmap")
-ggmap(map) +stat_density2d(data = df_3,
-                           aes(x = lon, y=lat,fill = ..level.., alpha = ..level..),
-                           size = 0.01, bins = 16, geom = "polygon") +scale_fill_gradient(low = "green", high = "red",guide = FALSE)+scale_alpha(range = c(0, 0.5), guide = FALSE)
+# get some statistics of each district in Taipei between 201501~201708
+df %>% group_by(district) %>% summarise(m=n()) %>% arrange(.,-m)
 
-# df_4: 台北市萬華區
-map <- get_map(location = c(lon=median(df_4$lon),lat=median(df_4$lat)),
-               source = "google", 
-               zoom = 14,
-               language = "zh-TW", 
-               maptype = "roadmap")
-ggmap(map) +stat_density2d(data = df_4, aes(x = lon, y=lat,fill = ..level.., alpha = ..level..),size = 0.01, bins = 16, geom = "polygon") +scale_fill_gradient(low = "green", high = "red",guide = FALSE)+scale_alpha(range = c(0, 0.5), guide = FALSE)
-
-# df_5: 台北市松山區
-map <- get_map(location = c(lon=median(df_5$lon),lat=median(df_5$lat)),
-               source = "google", 
-               zoom = 14,
-               language = "zh-TW", 
-               maptype = "roadmap")
-ggmap(map) +stat_density2d(data = df_5, aes(x = lon, y=lat,fill = ..level.., alpha = ..level..),size = 0.01, bins = 16, geom = "polygon") +scale_fill_gradient(low = "green", high = "red",guide = FALSE)+scale_alpha(range = c(0, 0.5), guide = FALSE)
-
-
-# df_7: 台北市信義區
-map <- get_map(location = c(lon=median(df_7$lon),lat=median(df_7$lat)),
-               source = "google", 
-               zoom = 14,
-               language = "zh-TW", 
-               maptype = "roadmap")
-ggmap(map) +stat_density2d(data = df_7, aes(x = lon, y=lat,fill = ..level.., alpha = ..level..),size = 0.01, bins = 16, geom = "polygon") +scale_fill_gradient(low = "green", high = "red",guide = FALSE)+scale_alpha(range = c(0, 0.5), guide = FALSE)
